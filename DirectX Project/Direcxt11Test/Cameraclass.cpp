bc53b5cfefc0df6b2834bf29c85555d872a7023c
cmscms419 <alstjr123qwe@gmail.com>
2021-05-22 01:22:38 +0900

    Updata
    DirectX11 Test
    Buffer
    Shader
    HLSL

---
+++ DirectX Project/Direcxt11Test/Cameraclass.cpp
@@ -0,0 +1,96 @@
+#include "Stdafx.h"
+#include "Cameraclass.h"
+
+CameraClass::CameraClass()
+{
+	m_position = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	m_rotation = XMFLOAT3(0.0f, 0.0f, 0.0f);
+}
+
+CameraClass::CameraClass(const CameraClass& other)
+{
+
+}
+
+CameraClass::~CameraClass()
+{
+
+}
+
+void CameraClass::SetPosition(float x, float y, float z)
+{
+	m_position.x = x;
+	m_position.y = y;
+	m_position.z = z;
+}
+
+void CameraClass::SetRotation(float x, float y, float z)
+{
+	m_rotation.x = x;
+	m_rotation.y = y;
+	m_rotation.z = z;
+}
+
+XMFLOAT3 CameraClass::GetPosition()
+{
+	return m_position;
+}
+
+XMFLOAT3 CameraClass::GetRotation()
+{
+	return m_rotation;
+}
+
+void CameraClass::Render()
+{
+	XMFLOAT3 up, position, lookAT;
+	XMVECTOR upVector, positionVector, lookAtVector;
+	float yaw, pitch, roll;
+	XMMATRIX rotationMatrix;
+
+
+	// 위쪽을 가리키는 벡터를 설정합니다.
+	up.x = 0.0f;
+	up.y = 1.0f;
+	up.z = 0.0f;
+
+	// XMVECTOR 구조체에 로드한다.
+	upVector = XMLoadFloat3(&up);
+
+	// 3D월드에서 카메라의 위치를 설정합니다.
+	position = m_position;
+
+	// XMVECTOR 구조체에 로드한다.
+	positionVector = XMLoadFloat3(&position);
+
+	// 기본적으로 카메라가 찾고있는 위치를 설정합니다.
+	lookAT.x = 0.0f;
+	lookAT.y = 0.0f;
+	lookAT.z = 1.0f;
+
+	// XMVECTOR 구조체에 로드한다.
+	lookAtVector = XMLoadFloat3(&lookAT);
+
+	// yaw (y 축), pitch (x 축) 및 roll (z 축)의 회전값을 라디안 단위로 설정합니다.
+	pitch = m_rotation.x * 0.0174532925f;
+	yaw = m_rotation.y * 0.0174532925f;
+	roll = m_rotation.z * 0.0174532925f;
+
+	// yaw, pitch, roll 값을 통해 회전 행렬을 만듭니다.
+	rotationMatrix = XMMatrixRotationRollPitchYaw(pitch, yaw, roll);
+
+	//lookAt 및 up 벡터를 회전 행렬로 변형하여 뷰가 원점에서 올바르게 회전되도록 합니다.
+	lookAtVector = XMVector3TransformCoord(lookAtVector, rotationMatrix);
+	upVector - XMVector2TransformCoord(upVector, rotationMatrix);
+
+	// 회전 된 카메라 위치를 뷰어 위치로 변환합니다.
+	lookAtVector = XMVectorAdd(positionVector, lookAtVector);
+
+	//마지막으로 세 개의 엡데이트 된 벡터에서 뷰 행렬을 만듭니다.
+	m_viewMatrix = XMMatrixLookAtLH(positionVector, lookAtVector, upVector);
+}
+
+void CameraClass::GetViewMatrix(XMMATRIX& viewMatrix)
+{
+	viewMatrix = m_viewMatrix;
+}
